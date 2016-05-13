7d42a74d3fae0621dd359f87cc0e50798fc7046b
Ridhi <ridhi.129@gmail.com>
2016-05-13 18:25:10 -0500

    Added root project
    Skeleton root project added. Initial features added

---
+++ MyPortfolio/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyPortfolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View(); // using view template
+        }
+
+        public ActionResult WorkExperience()
+        {
+            //ViewBag.Message = "Your application description page.";
+
+
+            return View();
+        }
+
+        public ActionResult Education()
+        {
+            //ViewBag.Message = "Your application description page.";
+
+
+            return View();
+        }
+
+        public ActionResult Projects()
+        {
+            //ViewBag.Message = "Your application description page.";
+
+
+            return View();
+        }
+      
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Lets know more about each other on social media:";
+
+            return View();
+        }
+    }
+}
